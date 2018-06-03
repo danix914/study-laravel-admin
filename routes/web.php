820c19b0aa61609b75742b1c3ba814b593dd2ab9
Det Hsieh <danix914@gmail.com>
2018-06-03 13:59:03 +0800

    add routes for uploaded files from laravel-admin

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,18 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/cfiles/images/{filename}', function ($filename) {
+    $path = storage_path('uploads/images/' . $filename);
+    if (!File::exists($path)) {
+        abort(404);
+    }
+
+    $file = File::get($path);
+    $type = File::mimeType($path);
+
+    $response = Response::make($file, 200);
+    $response->header("Content-Type", $type);
+
+    return $response;
+});
